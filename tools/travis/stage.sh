0021bee2a6f7bf81ee59c89a8248f4d92923a3ea
zentol <chesnay@apache.org>
2019-01-17 15:08:04 +0100

    skip flink-s3-fs-hadoop

--- tools/travis/stage.sh
+++ tools/travis/stage.sh
@@ -51,7 +51,6 @@ flink-queryable-state/flink-queryable-state-client-java"
 MODULES_CONNECTORS="\
 flink-contrib/flink-connector-wikiedits,\
 flink-filesystems/flink-s3-fs-base,\
-flink-filesystems/flink-s3-fs-hadoop,\
 flink-filesystems/flink-s3-fs-presto,\
 flink-formats/flink-parquet,\
 flink-connectors/flink-hcatalog,\
@@ -78,6 +77,7 @@ MODULES_EXCLUDE_FAILING="\
 !flink-runtime,\
 !flink-scala,\
 !flink-filesystems/flink-hadoop-fs,\
+!flink-filesystems/flink-s3-fs-hadoop,\
 !flink-filesystems/flink-mapr-fs,\
 !flink-connectors/flink-connector-elasticsearch,\
 !flink-tests,\
